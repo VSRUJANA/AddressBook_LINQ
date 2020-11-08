8a560c1fb96f2dbd716bffc5134c8f5641c2d6c8
VSRUJANA <srujana.valavala@gmail.com>
2020-11-08 11:17:38 +0530

    [srujana] Add. Create Address Book Data table

---
+++ AddressBook_LINQ/ManageAddressBook.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace AddressBook_LINQ
+{
+    class ManageAddressBook
+    {
+        // Create Data table to store address book details
+        public DataTable dataTable = new DataTable();
+    }
+}
